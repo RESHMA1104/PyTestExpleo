217b04a1864a75123e67ca6706fc6074bca2e08b
RESHMMA1104 <resh1104mcdc@gmail.com>
2026-06-10 23:19:56 +0530

    Relative Locators

--- RelativeLocators/read_config.py
+++ RelativeLocators/read_config.py
@@ -1,6 +1,14 @@
 from configparser import ConfigParser
+import os
 
 def get_config(category, key):
     config = ConfigParser()
-    config.read("./config.ini")
+
+    config_path = os.path.join(
+        os.path.dirname(__file__),
+        "config.ini"
+    )
+
+    config.read(config_path)
+
     return config.get(category, key)
